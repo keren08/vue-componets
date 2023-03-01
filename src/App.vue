d92bb9c77dc72335afac54afea4f080601849b6b
keren ramos <keren.ramoss@gmail.com>
2023-02-28 20:21:48 -0600

    componente dinamico

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,24 @@
 </script>
 
 <template>
-<book-info></book-info>
+<ul>
+<book-info
+    id="1"
+    title="Orgullo y prejuicio"
+    author="Jane Austin"
+    realse-date="1813-01-28"
+    isLike ='1'
+    >
+</book-info>
+<book-info
+    id="2"
+    title="Orgullo y prejuicio 2"
+    author="Jane Austin"
+    realse-date="1813-01-28"
+    isLike ='0'
+    >
+</book-info>
+</ul>
 </template>
 
 <style scoped>
